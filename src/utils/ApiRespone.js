51ddf6b8cb0e2ab61fbc5a366ad47752cb058c70
MaazMuhammad2 <maaz86507@gmail.com>
2024-07-28 01:54:20 +0500

    util code

---
+++ src/utils/ApiRespone.js
@@ -0,0 +1,8 @@
+class ApiResponse{
+    constructor(stausCode, message = "Success", data){
+        this.stausCode = stausCode
+        this.data = data
+        this.message = message
+        this.success = stausCode < 400
+    }
+}
