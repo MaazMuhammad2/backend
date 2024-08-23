0fd58711db8b07b6ccae60e3b6f2879839aee0f5
MaazMuhammad2 <maaz86507@gmail.com>
2024-08-23 23:12:39 +0500

    add new routes for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,13 @@ import {
   loginUser,
   logoutUser,
   refreshAccessToken,
+  currentUserPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -11,16 +18,16 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
 router.route("/register").post(
-//   upload.fields([
-//     {
-//       name: "avatar",
-//       maxCount: 1,
-//     },
-//     {
-//       name: "coverImage",
-//       maxCount: 1,
-//     },
-//   ]),
+  //   upload.fields([
+  //     {
+  //       name: "avatar",
+  //       maxCount: 1,
+  //     },
+  //     {
+  //       name: "coverImage",
+  //       maxCount: 1,
+  //     },
+  //   ]),
   registerUser
 );
 
@@ -28,6 +35,18 @@ router.route("/login").post(loginUser);
 
 // secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
-router.route("/refresh-token").post(refreshAccessToken)
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/changePassword").post(verifyJWT, currentUserPassword);
+router.route("/current-user").post(verifyJWT, getCurrentUser);
+router.route("/update-account-detail").patch(verifyJWT, updateAccountDetails); // patch means it will only change one thing not the whole
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+// taking data from params
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getWatchHistory);
 
 export default router;
